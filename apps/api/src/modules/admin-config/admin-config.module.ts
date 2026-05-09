471324d8f27ab78789be3194cd8bead9a1e22fdc
alesz1296-dev <alesz1296@gmail.com>
2026-05-08 21:27:51 -0600

    railway deplyment

--- apps/api/src/modules/admin-config/admin-config.module.ts
+++ apps/api/src/modules/admin-config/admin-config.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { CatalogsModule } from "../catalogs/catalogs.module";
 import { IdentityAccessModule } from "../identity-access/identity-access.module";
 import { AdminConfigController } from "./admin-config.controller";
 import { AdminConfigService } from "./admin-config.service";
 
 @Module({
-  imports: [CatalogsModule, IdentityAccessModule],
+  imports: [AuthModule, CatalogsModule, IdentityAccessModule],
   controllers: [AdminConfigController],
   providers: [AdminConfigService]
 })
